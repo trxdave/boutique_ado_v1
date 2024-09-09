395b742de786fea22c7e95562de4ebf22ec8709f
David Cotter <daveguy28@gmail.com>
2024-09-09 13:35:54 +0000

    added checkout success logic

--- boutique/settings.py
+++ boutique/settings.py
@@ -16,6 +16,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+try:
+    import env
+except ImportError:
+    pass
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -192,6 +197,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
-STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_CURRENCY = os.getenv('STRIPE_CURRENCY', 'usd')
