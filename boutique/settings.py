5e56140d4d3690c7f3061ab9f1af80f829273e75
David Cotter <daveguy28@gmail.com>
2024-09-16 13:21:11 +0000

    Added cache control

--- boutique/settings.py
+++ boutique/settings.py
@@ -218,6 +218,12 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'boutique-ado-dc'
     AWS_S3_REGION_NAME = 'eu-north-1'
